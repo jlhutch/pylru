52fa54a4715705554ee3941e60d00ae0bdb9e773
Jay Hutchinson <jlhutch@gmail.com>
2010-10-10 13:22:38 -0500

    Updated version to 1.0.1 in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name = "pylru",
-    version = "1.0.0",
+    version = "1.0.1",
     py_modules=['pylru'],
     description = "A least recently used (LRU) cache implementation",
     author = "Jay Hutchinson",
