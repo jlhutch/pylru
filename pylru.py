eca02544df4153deed1199af06ccdcdd51bd0f87
Jay Hutchinson <jlhutch@gmail.com>
2015-03-24 13:15:47 -0500

    Fixes #13, a bug in lrudecorator.

--- pylru.py
+++ pylru.py
@@ -535,8 +535,22 @@ import functools
 
 class lrudecorator(object):
     def __init__(self, size):
-        self.size = size
+        self.cache = lrucache(size)
 
     def __call__(self, func):
-        wrapper = FunctionCacheManager(func, self.size)
+        def wrapper(*args, **kwargs):
+            kwtuple = tuple((key, kwargs[key]) for key in sorted(kwargs.keys()))
+            key = (args, kwtuple)
+            try:
+                return self.cache[key]
+            except KeyError:
+                pass
+
+            value = func(*args, **kwargs)
+            self.cache[key] = value
+            return value
+
+        wrapper.cache = self.cache
+        wrapper.size = self.cache.size
+        wrapper.clear = self.cache.clear
         return functools.update_wrapper(wrapper, func)
